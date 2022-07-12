f3fd72f0c0e16daf3259c7c0a3eb612b1db12315
denisebosisio <denisebosisio12@gmail.com>
2022-07-12 10:59:04 -0300

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-const [input0, input1, input2] = document.querySelectorAll("input");
-/*const botao = document.querySelectorAll(button);*/
+const [input0, input1] = document.querySelectorAll("input");
 const imagem = document.querySelectorAll("image 1");
+const botao = document.querySelectorAll(button);
+
+function habilitar(){
+    if(input0.value && input1.value.length >= 8){
+        botao.removeAttribute("disabled");
+    } else{
+        botao.setAttribute("disabled", "disabled");
+    }
+}
+input0.addEventListener("input", habilitar);
+input1.addEventListener("input", habilitar);
